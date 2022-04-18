3e72fe144a240af908844dba6a28e2f8e67f8e8b
Eduardo Beltran <edd.bng@gmail.com>
2022-04-18 07:16:03 -0500

    Se agregó css a certificados

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import HeadSection from './UI/HeadSection';
+
 import Logo1 from '../assets/icons/logo-ADCI.jpg'
 import Logo2 from '../assets/icons/logo-industria-limpia.jpg'
 import Logo3 from '../assets/icons/logo-AENOR-1.png'
@@ -8,15 +10,15 @@ import Logo5 from '../assets/icons/logo-AENOR-2.png'
 
 function Certificates() {
   return (
-    <section className='block bro'>
+    <section className='block--big'>
         <div className='small-container'>
-          <h2 className='block__header'>Certificates</h2>
+          <HeadSection title="Certificaciones" textStyle="primary"/>
           <div className='certificate'>
-            <div className='certificate__logo'><img src={Logo1} /></div>
-            <div className='certificate__logo'><img src={Logo2} /></div>
-            <div className='certificate__logo'><img src={Logo3} /></div>
-            <div className='certificate__logo'><img src={Logo4} /></div>
-            <div className='certificate__logo'><img src={Logo5} /></div>
+            <div className='certificate__logo'><img src={Logo1} alt="ADCI" /></div>
+            <div className='certificate__logo'><img src={Logo2} alt="Industria Limpia"/></div>
+            <div className='certificate__logo'><img src={Logo3} alt="AENOR" /></div>
+            <div className='certificate__logo'><img src={Logo4} alt="ESR" /></div>
+            <div className='certificate__logo'><img src={Logo5} alt="AENOR" /></div>
           </div>
         </div>
     </section>
